58d7b6ead5ad37242a03ff712f173c02bdd845f6
Mauricio Giraldo <mauricio.giraldo@gmail.com>
2015-10-21 19:55:05 -0400

    Initial Commit

---
+++ bitpix-move/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bitpix-move
+//
+//  Created by Mauricio Giraldo on 21/10/15.
+//  Copyright © 2015 Ping Pong Estudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ bitpix-move/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bitpix-move
+//
+//  Created by Mauricio Giraldo on 21/10/15.
+//  Copyright © 2015 Ping Pong Estudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
